93393b8bc32ae0667076b80a83cb2fd64a255e8f
nazar <nhavronskyi@gmail.com>
2023-09-11 17:41:15 +0200

    refactored

--- src/main/java/com/jobs_scout/pracujplparser/service/impl/UrlServiceImpl.java
+++ src/main/java/com/jobs_scout/pracujplparser/service/impl/UrlServiceImpl.java
@@ -1,12 +1,10 @@
 package com.jobs_scout.pracujplparser.service.impl;
 
 import com.jobs_scout.pracujplparser.service.UrlService;
-import lombok.SneakyThrows;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UrlServiceImpl implements UrlService {
-    @SneakyThrows
     @Override
     public String getUrl(String tag) {
         return "https://it.pracuj.pl/praca/" + tagConverter(tag);
